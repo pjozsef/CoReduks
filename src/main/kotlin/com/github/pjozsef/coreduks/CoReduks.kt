f4fd258bf571f3e776d289ad814ae0e924eb157d
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-11-25 11:58:23 +0100

    feat(coreduks): add generics for action as well

--- src/main/kotlin/com/github/pjozsef/coreduks/CoReduks.kt
+++ src/main/kotlin/com/github/pjozsef/coreduks/CoReduks.kt
@@ -4,30 +4,30 @@ import kotlinx.coroutines.*
 import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
 
-interface Reducer<T> {
-    operator fun invoke(currentState: T, action: Any): T
+interface Reducer<S, A> {
+    operator fun invoke(currentState: S, action: A): S
 }
 
-interface Subscriber<T> {
-    fun onNewState(newState: T)
+interface Subscriber<S> {
+    fun onNewState(newState: S)
 }
 
-interface Middleware<T> {
-    operator fun invoke(store: Store<T>, action: Any, next: (Any) -> T): T
+interface Middleware<S, A> {
+    operator fun invoke(store: Store<S, A>, action: A, next: (A) -> S): S
 }
 
-interface Store<T> {
-    val state: T
-    fun dispatch(action: Any)
+interface Store<S, A> {
+    val state: S
+    fun dispatch(action: A)
 }
 
 
-class CoReduksStore<T> @JvmOverloads constructor(
-        initialState: T,
-        reducer: Reducer<T>,
-        middlewares: List<Middleware<T>> = listOf(),
+class CoReduksStore<S, A> @JvmOverloads constructor(
+        initialState: S,
+        reducer: Reducer<S, A>,
+        middlewares: List<Middleware<S, A>> = listOf(),
         val scope: CoroutineScope = DEFAULT_SCOPE,
-        val defaultSubscriberContext: CoroutineContext = scope.coroutineContext) : Store<T> {
+        val defaultSubscriberContext: CoroutineContext = scope.coroutineContext) : Store<S, A> {
 
     companion object {
         val DEFAULT_SCOPE: CoroutineScope by lazy {
@@ -48,32 +48,32 @@ class CoReduksStore<T> @JvmOverloads constructor(
         }
     }
 
-    private inner class StoreProxy : Store<T> {
-        override val state: T
+    private inner class StoreProxy : Store<S, A> {
+        override val state: S
             get() = this@CoReduksStore.currentState
 
-        override fun dispatch(action: Any) {
+        override fun dispatch(action: A) {
             this@CoReduksStore.dispatch(action)
         }
     }
 
-    private var currentState: T = initialState
+    private var currentState: S = initialState
     private val proxy: StoreProxy = StoreProxy()
-    private val subscribers: MutableMap<Subscriber<T>, CoroutineContext> = hashMapOf()
-    private val dispatchFunction: (Any) -> T = createDispatchFunction(middlewares.asReversed()) {
+    private val subscribers: MutableMap<Subscriber<S>, CoroutineContext> = hashMapOf()
+    private val dispatchFunction: (A) -> S = createDispatchFunction(middlewares.asReversed()) {
         reducer(currentState, it)
     }
 
-    override val state: T
+    override val state: S
         get() = runBlocking { stateAsync.await() }
 
-    val stateAsync: Deferred<T>
+    val stateAsync: Deferred<S>
         get() = scope.async { currentState }
 
-    override fun dispatch(action: Any) {
+    override fun dispatch(action: A) {
         scope.launch {
             val newState = dispatchFunction(action)
-            if(currentState != newState){
+            if (currentState != newState) {
                 currentState = newState
                 for ((subscriber, context) in subscribers) {
                     withContext(context) {
@@ -84,7 +84,7 @@ class CoReduksStore<T> @JvmOverloads constructor(
         }
     }
 
-    fun subscribe(subscriber: Subscriber<T>, context: CoroutineContext = defaultSubscriberContext) {
+    fun subscribe(subscriber: Subscriber<S>, context: CoroutineContext = defaultSubscriberContext) {
         scope.launch {
             subscribers[subscriber] = context
             withContext(context) {
@@ -93,19 +93,19 @@ class CoReduksStore<T> @JvmOverloads constructor(
         }
     }
 
-    fun unsubscribe(subscriber: Subscriber<T>) {
+    fun unsubscribe(subscriber: Subscriber<S>) {
         scope.launch {
             subscribers.remove(subscriber)
         }
     }
 
-    private tailrec fun createDispatchFunction(middlewares: List<Middleware<T>>, currentDispatch: (Any) -> T): (Any) -> T =
+    private tailrec fun createDispatchFunction(middlewares: List<Middleware<S, A>>, currentDispatch: (A) -> S): (A) -> S =
             when (middlewares.size) {
                 0 -> currentDispatch
                 else -> {
                     val head = middlewares.first()
                     val tail = middlewares.subList(1, middlewares.size)
-                    val nextDispatch: (Any) -> T = {
+                    val nextDispatch: (A) -> S = {
                         head(proxy, it, currentDispatch)
                     }
                     createDispatchFunction(tail, nextDispatch)
