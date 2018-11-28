e96be8d7ec8d0c499d57e0358d0755c08d411472
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-11-28 23:13:34 +0100

    feat(middleware): add middleware for logging actions and state transitions

---
+++ src/main/kotlin/com/github/pjozsef/coreduks/middleware/CoReduksLoggingMiddleware.kt
@@ -0,0 +1,14 @@
+package com.github.pjozsef.coreduks.middleware
+
+import com.github.pjozsef.coreduks.Middleware
+import com.github.pjozsef.coreduks.Store
+
+class CoReduksLoggingMiddleware<S, A>: Middleware<S, A> {
+    override fun invoke(store: Store<S, A>, action: A, next: (A) -> S): S {
+        println("CoReduksLogginMiddleware: old state: ${store.state}")
+        println("CoReduksLogginMiddleware: action: $action")
+        val newState = next(action)
+        println("CoReduksLogginMiddleware: new state: $newState\n")
+        return newState
+    }
+}
